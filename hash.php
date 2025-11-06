534b537c693f2bc23dfe47b461d451ba384caf90
timkipp <timkipp@icloud.com>
2025-11-06 18:40:21 -0500

    Remove deleted files and normalize line endings

--- hash.php
+++
@@ -1,4 +0,0 @@
-<?php
-    $password = "B@nkOfTh3Un1t3dSt@t3$";
-    echo password_hash($password, PASSWORD_DEFAULT);
-?>
--- scripts/debug.js
+++
@@ -1,8 +0,0 @@
-export function initializeDebugging() {
-    // console.clear();
-}
-
-export function delay(ms) {
-    console.warn("💩 SETTING DELAY TO DEAL WITH BROWSER BS");
-    return new Promise((resolve) => setTimeout(resolve, ms));
-}
